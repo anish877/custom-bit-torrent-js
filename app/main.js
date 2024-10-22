beb215127bc1600b1ae45a53dcb80a40664ffea1
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-22 23:57:30 +0530

    stage 2 decode lists

--- app/main.js
+++ app/main.js
@@ -19,11 +19,11 @@ function decodeBencode(bencodedValue) {
     else if(bencodedValue[0]==='l' && bencodedValue[bencodedValue.length-1]==='e'){
       const bencodedElements = bencodedValue.slice(0,bencodedValue.length-1)
       console.log(bencodedElements)
-      if(bencodedElements=""){
+      if(bencodedElements===""){
         return []
       }
       else{
-        
+
       }
     }
     else{
