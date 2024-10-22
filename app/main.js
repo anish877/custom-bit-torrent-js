0578405d4ff3d70903fc7fe12a3ecdbc970b4be6
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-22 11:20:58 +0530

    stage 1 decode integers

--- app/main.js
+++ app/main.js
@@ -14,7 +14,7 @@ function decodeBencode(bencodedValue) {
     return bencodedValue.substr(firstColonIndex + 1);
   } else {
     if(bencodedValue[0]==='i' && bencodedValue[bencodedValue.length-1]==='e'){
-      return(bencodedValue.slice(1,-1))
+      return(parseInt(bencodedValue.slice(1,-1)))
     }
 
     else{
