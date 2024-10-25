e93706d5a66172c49632c6e4e4e73a06788d8046
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-25 07:27:50 +0530

    stage 2 decode lists

--- app/main.js
+++ app/main.js
@@ -24,28 +24,24 @@ function decodeBencode(bencodedValue) {
       }
       else{
         while(true){
-          let index = 0
-          const indexOfColon = bencodedElements.indexOf(':',index)
-          const indexOfi = bencodedElements.indexOf(!NaN,index)-1
-          if(indexOfColon===-1 && indexOfi===-1){break}
-          if(indexOfColon<indexOfi && indexOfColon!==-1 && indexOfi!==-1){
+          const indexOfColon = bencodedElements.indexOf(':')
+          const indexOfi = bencodedElements.indexOf('i')
+          if(indexOfColon===-1&&indexOfi===-1) break
+          if(indexOfColon===1){
             const lengthOfString = parseInt(bencodedElements.charAt(indexOfColon-1))
             const encodedString = bencodedElements.slice(indexOfColon-1,indexOfColon+lengthOfString+1)
             const decodedString = decodeBencode(encodedString)
-            index = indexOfColon+lengthOfString+1
+            bencodedElements = bencodedElements.slice(0,indexOfColon+lengthOfString+1)
             list.push(decodedString)
-            console.log(index,encodedString,decodedString)
           }
-          else if(indexOfi<indexOfColon && indexOfColon!==-1 && indexOfi!==-1){
+          else if(indexOfi===0){
             const indexOfe = bencodedElements.indexOf('e')
-            const endcodedInteger = bencodedElements.slice(indexOfi,indexOfe+1)
-            const decodedInteger = decodeBencode(endcodedInteger)
-            index = indexOfe + 1
+            const encodedInteger = bencodedElements.slice(indexOfi,indexOfe+1)
+            const decodedInteger = decodeBencode(encodedInteger)
+            bencodedElements = bencodedElements.slice(0,indexOfe+1)
             list.push(decodedInteger)
-            console.log(index,decodedInteger,endcodedInteger)
           }
         }
-
         return list
       }
     }
