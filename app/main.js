7a3bb93b72359f4283366f59faf9169ecd9a33a7
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-25 23:21:08 +0530

    stage 4 parse torrent file

--- app/main.js
+++ app/main.js
@@ -170,8 +170,8 @@ function main() {
   }else if(command==='info'){
     const file = process.argv[3]
     const content = fs.readFileSync(path.resolve(process.cwd(),file))
-    console.log(content)
-    const decodedContent = decodeBencode(content)
+    console.log(content.toString())
+    const decodedContent = decodeBencode(content.toString())
     console.log(`Tracker URL: ${decodedContent['announce']}`)
     console.log(`Length: ${decodedContent['info']['length']}`)
   }
