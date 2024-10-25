db5ed75440e054c6fb22c66e29521ea09be1e2a2
anish877 <62011810+anish877@users.noreply.github.com>
2024-10-26 00:01:27 +0530

    stage 5 info hash

--- app/main.js
+++ app/main.js
@@ -173,7 +173,7 @@ function main() {
     const file = process.argv[3]
     const content = fs.readFileSync(path.resolve(process.cwd(),file)).toString()
     const decodedContent = decodeBencode(content.toString())
-    process.stdout.write(`Length: ${decodedContent['info']['length']}`)
+    process.stdout.write(`Length: ${decodedContent['info']}`)
   }
    else {
     throw new Error(`Unknown command ${command}`);
